c19e44d5e6b54275d35bb26c7ed272558c3d1576
breonwilliams <bjwilli83@gmail.com>
2022-11-21 08:50:21 -0500

    add bootstrap

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '0297a9600b922bbe596e284c68f5c5b949393497',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '0297a9600b922bbe596e284c68f5c5b949393497',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twig/twig' => array(
+            'pretty_version' => 'v1.13.2',
+            'version' => '1.13.2.0',
+            'reference' => '6d6a1009427d1f398c9d40904147bf9f723d5755',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twig/twig',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
